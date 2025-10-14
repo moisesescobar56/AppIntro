7f9585c1ef07bbd9176555c7c1dca1062f44b68c
Moises Escobar <moises.escobar@esfe.agape.edu.sv>
2025-10-13 20:15:50 -0600

    Input Component

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CalculadoraScreen from './screens/CalculadoraScreen';
-import { Layout } from './components';
+import { Layout , Input } from './components';
 
 export default function App() {
   return (
     <Layout title="Contacto" >
-      
+      <Input label="Email:" placeholder="name@mail.com" type="email-addres" />
+      <Input label="Asunto:" placeholder="Consulta" />
+      <Input label="Mensaje:" placeholder="Escribir aqui..." lines={10}/>
     </Layout>
   );
 }
+    
